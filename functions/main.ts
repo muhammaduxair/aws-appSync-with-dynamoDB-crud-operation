935d3650e43e0e7a9e5c5c6b1897e119f129bf42
muhammaduxair <uxairjaaan@gmail.com>
2021-06-11 10:09:25 +0500

    aws appSync data sources lambda & dynamoDB working on graphql crud operation

---
+++ functions/main.ts
@@ -0,0 +1,30 @@
+import addTodo from "./addTodo";
+import getTodos from "./getTodos";
+import updateTodo from "./updateTodo";
+import deleteTodo from "./deleteTodo";
+import Todo from "./todoType";
+
+type AppSyncEvent = {
+  info: {
+    fieldName: string;
+  };
+  arguments: {
+    todoId: string;
+    todo: Todo;
+  };
+};
+
+exports.handler = async (event: AppSyncEvent) => {
+  switch (event.info.fieldName) {
+    case "addTodo":
+      return await addTodo(event.arguments.todo);
+    case "getTodos":
+      return await getTodos();
+    case "deleteTodo":
+      return await deleteTodo(event.arguments.todoId);
+    case "updateTodo":
+      return await updateTodo(event.arguments.todo);
+    default:
+      return null;
+  }
+};
